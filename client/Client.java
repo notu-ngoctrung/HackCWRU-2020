78f24d5ca003b7d030a987c2d8de94971d446555
Trung Nguyen Huynh <rezolden@gmail.com>
2020-02-09 09:13:07 -0500

    Add files via upload
    Fixes some bugs

--- client/Client.java
+++ client/Client.java
@@ -7,124 +7,124 @@ import java.awt.image.BufferedImage;
 import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.Toolkit;
+import java.awt.Dimension;
 import javax.imageio.ImageIO;
-import javafx.appplication.application
+import javafx.application.Application;
 import javafx.stage.Stage;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.*;
 import javafx.scene.control.*;
+import javafx.geometry.*;
+import javafx.scene.*;
+import javafx.scene.text.*;
+import javafx.event.*;
 
 public class Client extends Application  {
-    private static final int PORT = 6969;
-	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-	private static final int WIDTH = screenSize.getWidth();
-	private static final int HEIGHT = screenSize.getHeight();
-	private static String ADDRESS = "localhost"
-	public static final String FIRST_NAME = "Unimote"
-	public static final String LAST_NAME = "HackCWRU 2020"
-	private static Robot robot;
-	private static boolean isActive = false;
-
-	@Override
-    public static void start(Stage primaryStage) throws UnknownHostException, IOException, AWTException {
-		robot = new Robot();
-        Command command = new Command(Command.CType.HAND, FIRST_NAME, LAST_NAME, WIDTH, HEIGHT);
-        Socket client = new Socket(ADDRESS, PORT);
-		DataInputStream input = new DataInputStream(client.getInputStream());
-        DataOutputStream output = new DataOutputStream(client.getOutputStream());
-
-		primaryStage.setTitle("Student");
-		GridPane gp = new GridPane();
-		gp.setAlignment(Pos.CENTER);
-		gp.setHgap(10);
-		gp.setVgap(10);
-		gp.setPadding(new Insets(25, 25, 25, 25));
-
-		Text scenetile = new Text("Unimote Client");
-		scenetile.setFont(Font.font("Arial", FontWeight.NORMAL, 18));
-		grid.add(scenetile, 0, 0, 2, 1);
-
-		Label userName = new Label("Server IP");
-		grid.add(userName, 0, 1);
-		TextField userTextField = new TextField(); //important
-		grid.add(userTextField, 1, 1);
-		Label pw = new Label("Port");
-		grid.add(pw, 0, 2);
-		TextField portBox = new TextFiledField(); //important
-		grid.add(portBox, 1, 2);
-
-		Button btn = new Button("Connect");
-		Button disconnectbtn = new Button("Disconnect");
-		HBox hbBtn = new HBox(10);
-		hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
-		hbBtn.getChildren().add(btn);
-		hbBtn.getChildren().add(disconnectbtn);
-		grid.add(hbBtn, 1, 4);
-
-		btn.setOnAction(new EventHandler<ActionEvent>() {
-				@Override
-				public void handle(ActionEvent e) {
-						output.write(Command.intToByteArray(command.getBytes().length));
-						output.write(command.getBytes());
-						output.flush();
-				}
-		});
-
-		disconnectbtn.setOnAction(new Eventhandler<ActionEvent>() {
-				@Override
-				public void handle(ActionEvent e) {
-						Command gbye = new Command("GBYE");
-						output.write(Command.intToByteArray(gbye.getBytes().length));
-						output.write(gbye.getBytes());
-						output.flush();
-				}
-		});
-
-		Scene scene = new Scene(gp, 300, 300);
-		primaryStage.setScene(scene);
-		primaryStage.show();
-
-		//output.write(Command.intToByteArray(command.getBytes().length));
-        //output.write(command.getBytes());
-		//output.flush();
-        
-		BufferedImage image;
-		ByteArrayOutputStream bos = new ByteArrayOutputStream();
-		byte[] imageBytes, data;
-		while (true) {
-			if (!isActive) {
-				data = new byte[input.readInt()];
-				input.readFully(data, 0, data.length);
-				command = new Command(new String(data))
-				if (command.getType() == "STRT") {
-					isActive = true;
-					System.out.println("Starting remote control");
-				}
-				else
-					continue;
-			}
-			// Send screen buffer to controller (server)
-			image = robot.createScreenCapture(new Rectangle(0, 0, WIDTH, HEIGHT));
-			ImageIO.write(image, "jpg", bos);
-			imageBytes = bos.toByteArray();
-			command = new Command(Command.CType.SCRN, imageBytes.length);
-			byte[] temp = command.getBytes();
-			output.write(Command.intToByteArray(temp.length));
-			output.write(temp);
-			output.write(imageBytes);
-			bos.reset(); // to make room for next BufferedImage
-
-			data = new byte[input.readInt()];
-			input.readFully(data, 0, data.length);
-			command = new Command(new String(data));
-			String[] arg = command.getArgs();
-			switch (command.getType()) {
-				case "MOVE": move(Integer.parseInt(arg[0]), Integer.parseInt(arg[1])); break;
-				case "CLCK": click(arg[0] == "0" ? false : true, Integer.parseInt(arg[1])); break;
-				case "KEYB": keyboard(arg[0] == "0" ? false : true, Integer.parseInt(arg[1])); break;
-				case "GBYE": isActive = false; break;
-				default: break;
-			}
+  private static int PORT = 6969;
+//  Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+  private static int WIDTH;
+  private static int HEIGHT;
+  private static String ADDRESS = "localhost";
+  public static final String FIRST_NAME = "Unimote";
+  public static final String LAST_NAME = "HackCWRU 2020";
+  private static Robot robot;
+  private static boolean isActive = true;
+  
+  @Override
+  public void start(Stage primaryStage) throws UnknownHostException, IOException, AWTException {
+    robot = new Robot();
+    WIDTH = 1920;
+    HEIGHT = 1080;
+    Command command = new Command(Command.CType.HAND, FIRST_NAME, LAST_NAME, Integer.toString(WIDTH), Integer.toString(HEIGHT));
+    Socket client = new Socket(ADDRESS, PORT);
+    DataInputStream input = new DataInputStream(client.getInputStream());
+    DataOutputStream output = new DataOutputStream(client.getOutputStream());
+    
+    
+    primaryStage.setTitle("Student");
+    GridPane gp = new GridPane();
+    gp.setAlignment(Pos.CENTER);
+    gp.setHgap(10);
+    gp.setVgap(10);
+    gp.setPadding(new Insets(25, 25, 25, 25));
+    
+    Text scenetile = new Text("Unimote Client");
+    scenetile.setFont(Font.font("Arial", FontWeight.NORMAL, 18));
+    gp.add(scenetile, 0, 0, 2, 1);
+    
+    Label userName = new Label("Server IP");
+    gp.add(userName, 0, 1);
+    TextField userTextField = new TextField(); //important
+    gp.add(userTextField, 1, 1);
+    Label pw = new Label("Port");
+    gp.add(pw, 0, 2);
+    TextField portBox = new TextField(); //important
+    gp.add(portBox, 1, 2);
+    
+    Button btn = new Button("Connect");
+    Button disconnectbtn = new Button("Disconnect");
+    HBox hbBtn = new HBox(10);
+    hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+    hbBtn.getChildren().add(btn);
+    hbBtn.getChildren().add(disconnectbtn);
+    gp.add(hbBtn, 1, 4);
+    
+//    btn.setOnAction(new EventHandler<ActionEvent>() {
+//      @Override
+//      public void handle(ActionEvent e) {
+//        ADDRESS = userTextField.getText();
+//        PORT = Integer.parseInt(portBox.getText());
+//        try {
+//          Socket client = new Socket(ADDRESS, PORT);
+//        } catch (Exception err) {
+//          err.printStackTrace();
+//        }
+//        input = new DataInputStream(client.getInputStream());
+//        output = new DataOutputStream(client.getOutputStream());
+//        output.write(Command.intToByteArray(command.getBytes().length));
+//        output.write(command.getBytes());
+//        output.flush();
+//      }
+//    });
+    
+    disconnectbtn.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent e) {
+        if (client == null || !client.isConnected())
+          return;
+        Command gbye = new Command("GBYE");
+        try {
+          output.write(Command.intToByteArray(gbye.getBytes().length));
+          output.write(gbye.getBytes());
+          output.flush();
+        } catch (IOException err) {
+          err.printStackTrace();
         }
+      }
+    });
+    
+    Scene scene = new Scene(gp, 300, 300);
+    primaryStage.setScene(scene);
+//    primaryStage.show();
+    
+//    output.write(Command.intToByteArray(command.getBytes().length));
+//    output.write(command.getBytes());
+//    output.flush();
+    
+    BufferedImage image;
+    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+    byte[] imageBytes, data;
+    while (true) {
+      if (!isActive) {
+        data = new byte[input.readInt()];
+        input.readFully(data, 0, data.length);
+        command = new Command(new String(data));
+        if (command.getType().equals("STRT")) {
+          isActive = true;
+          System.out.println("Starting remote control");
+        }
+        else
+          continue;
+      }
       // Send screen buffer to controller (server)
       image = robot.createScreenCapture(new Rectangle(0, 0, WIDTH, HEIGHT));
       ImageIO.write(image, "jpg", bos);
@@ -133,20 +133,22 @@ public class Client extends Application  {
       byte[] temp = command.getBytes();
       output.write(Command.intToByteArray(temp.length));
       output.write(temp);
-	  output.flush();
       output.write(imageBytes);
       bos.reset(); // to make room for next BufferedImage
       
-      command = new Command(input.readLine());
+      data = new byte[input.readInt()];
+      input.readFully(data, 0, data.length);
+      command = new Command(new String(data));
       String[] arg = command.getArgs();
       switch (command.getType()) {
-        case "MOVE": move(Integer.parseInt(arg[0]), Integer.parseInt(arg[1]));
-        case "CLCK": click(arg[0] == "0" ? true : false, Integer.parseInt(arg[1]));
-        case "KEYB": keyboard(arg[0] == "0" ? true : false, Integer.parseInt(arg[1]));
-        case "GBYE": isActive = false;
+        case "MOVE": move(Integer.parseInt(arg[0]), Integer.parseInt(arg[1])); break;
+        case "CLCK": click(arg[0] == "0" ? false : true, Integer.parseInt(arg[1])); break;
+        case "KEYB": keyboard(arg[0] == "0" ? false : true, Integer.parseInt(arg[1])); break;
+        case "GBYE": isActive = false; break;
         default: break;
       }
     }
+    
   }
   
   public static void move(int x, int y) {
@@ -167,8 +169,8 @@ public class Client extends Application  {
     else
       robot.keyRelease(keycode); 
   }
-
+  
   public static void main(String[] args) throws Exception {
-    launch(args);		
+    launch(args);  
   }
 }
--- client/Command.java
+++ client/Command.java
@@ -1,82 +1,82 @@
 package client;
 
 public class Command {
-    CType type;
-    String[] cargs;
-    int length;
-
-    public enum CType {
-        HAND (4),
-        STRT (0), 
-        MOVE (2), 
-        CLCK (1), 
-        KEYB (2),
-        SCRN (1),
-        GBYE (0);
-
-        final int numArgs;
-        CType(int numArgs) {
-            this.numArgs = numArgs;
-        }
+  CType type;
+  String[] cargs;
+  int length;
+  
+  public enum CType {
+    HAND (4),
+      STRT (0), 
+      MOVE (2), 
+      CLCK (1), 
+      KEYB (2),
+      SCRN (1),
+      GBYE (0);
+    
+    final int numArgs;
+    CType(int numArgs) {
+      this.numArgs = numArgs;
     }
-
-    public Command(CType type, String... arg) {
-        this.type = type;
-        if (type.numArgs != arg.length)
-            throw new IllegalArgumentException("Command " + type.toString() + " needs " + type.numArgs + " arguments");
-        cargs = new String[type.numArgs];
-        for (int i=0; i<arg.length; ++i)
-            cargs[i] = Command.nameSanitize(arg[i]);
+  }
+  
+  public Command(CType type, String... arg) {
+    this.type = type;
+    if (type.numArgs != arg.length)
+      throw new IllegalArgumentException("Command " + type.toString() + " needs " + type.numArgs + " arguments");
+    cargs = new String[type.numArgs];
+    for (int i=0; i<arg.length; ++i)
+      cargs[i] = Command.nameSanitize(arg[i]);
+  }
+  
+  public Command(String string) {
+    String[] stuff = string.split(" ");
+    // Cross check types
+    cargs = new String[stuff.length - 1];
+    for (CType types : CType.values())
+      if (types.toString().equals(stuff[0]))
+      this.type = types;
+    for (int i=1; i<stuff.length; ++i)
+      cargs[i - 1] = stuff[i];
+  }
+  
+  public String stringify() {
+    StringBuilder s = new StringBuilder();
+    s.append(type.toString());
+    s.append(" ");
+    for (String args : cargs) {
+      s.append(args);
+      s.append(" ");
     }
-
-    public Command(String string) {
-        String[] stuff = string.split(" ");
-        // Cross check types
-		cargs = new String[stuff.length - 1];
-		for (CType types : CType.values())
-			if (types.toString().equals(stuff[0]))
-				this.type = types;
-		for (int i=1; i<stuff.length; ++i)
-			cargs[i - 1] = stuff[i];
-    }
-
-    public String stringify() {
-        StringBuilder s = new StringBuilder();
-        s.append(type.toString());
-        s.append(" ");
-        for (String args : cargs) {
-            s.append(args);
-            s.append(" ");
-        }
-        length = s.length();
-        return s.toString();
-    }
-
-    public byte[] getBytes() {
-        return this.stringify().getBytes();
-    }
-
-    private static String nameSanitize(String name) {
-        return name.replaceAll("\\s", "-");
-    }
-
-	public String getType() {
-		return type.toString();
-	}
-
-	public String[] getArgs() {
-		return cargs;
-	}
-
-	public static final byte[] intToByteArray(int value) {
-		return new byte[] { 
-			(byte)(value >>> 24),
-			(byte)(value >>> 16),
-			(byte)(value >>> 8), 
-			(byte)value}; 
-	}
-
-	public String getArgAt(int index) {	
-        return getArgs()[index];
-	}	
+    length = s.length();
+    return s.toString();
+  }
+  
+  public byte[] getBytes() {
+    return this.stringify().getBytes();
+  }
+  
+  private static String nameSanitize(String name) {
+    return name.replaceAll("\\s", "-");
+  }
+  
+  public String getType() {
+    return type.toString();
+  }
+  
+  public String[] getArgs() {
+    return cargs;
+  }
+  
+  public static final byte[] intToByteArray(int value) {
+    return new byte[] { 
+      (byte)(value >>> 24),
+        (byte)(value >>> 16),
+        (byte)(value >>> 8), 
+        (byte)value}; 
+  }
+  
+  public String getArgAt(int index) { 
+    return getArgs()[index];
+  } 
 }
